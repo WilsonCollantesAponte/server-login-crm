38819fb7a2c8c06d9a3acba6fe8b469113dad23a
Wilson Collantes Aponte <wilson.aponte000@gmail.com>
2023-11-15 12:53:17 -0500

    V3

--- app/user/api/route.js
+++ app/user/api/route.js
@@ -1,23 +1,36 @@
 import connection from "@/app/conexion/bd_conexion";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  connection.query(
-    // "SELECT * FROM clientes where id_cliente = ? ",
-    "SELECT * FROM users where user = ? AND clave = ? ",
+export async function POST(request) {
+  const { user, clave } = await request.json();
 
-    [
-      "leidy.huriol@negocia.pe",
-      "$2y$10$13mAvVh6R4ZHPUIMAzgHSOZII0jy/Qx83gTM0l3j3ZjRguba0ul0W",
-    ],
-
-    function (err, results, fields) {
-      var data = results;
-      alert(data);
-      console.log(results); // results contains rows returned by server
-      // console.log(fields); // fields contains extra meta data about results, if available
-    }
-  );
-
-  return NextResponse.json({ res: "wilson", response: "10" });
+  try {
+    const results = await queryDatabase(user, clave);
+    console.log(results);
+    return NextResponse.json(results);
+  } catch (error) {
+    console.error("Error en la consulta a la base de datos:", error);
+    return NextResponse.json({
+      error: "Hubo un error en la consulta a la base de datos",
+    });
+  }
 }
+
+function queryDatabase(user, clave) {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM users WHERE user = ? AND clave = ?",
+      [user, clave],
+      // function (err, results, fields) {
+      function (err, results) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
+}
+
+// Enviar mensaje a PRE-NEGOCIA - CRM
