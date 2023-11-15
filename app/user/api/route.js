ef568344a9885ac694af37cf52cee62dc57e4a79
AirtonYefri <pache_sagitario28_@hotmail.com>
2023-11-15 10:03:22 -0500

    mejora result

--- app/user/api/route.js
+++ app/user/api/route.js
@@ -2,22 +2,31 @@ import connection from "@/app/conexion/bd_conexion";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  connection.query(
-    // "SELECT * FROM clientes where id_cliente = ? ",
-    "SELECT * FROM users where user = ? AND clave = ? ",
+  try {
+    const results = await queryDatabase();
+    console.log(results);
+    return NextResponse.json({ res: results, response: "10" });
+  } catch (error) {
+    console.error('Error en la consulta a la base de datos:', error);
+    return NextResponse.json({ error: 'Hubo un error en la consulta a la base de datos' });
+  }
+}
 
-    [
-      "leidy.huriol@negocia.pe",
-      "$2y$10$13mAvVh6R4ZHPUIMAzgHSOZII0jy/Qx83gTM0l3j3ZjRguba0ul0W",
-    ],
-
-    function (err, results, fields) {
-      var data = results;
-      alert(data);
-      console.log(results); // results contains rows returned by server
-      // console.log(fields); // fields contains extra meta data about results, if available
-    }
-  );
-
-  return NextResponse.json({ res: "wilson", response: "10" });
+function queryDatabase() {
+  return new Promise((resolve, reject) => {
+    connection.query(
+      "SELECT * FROM users WHERE user = ? AND clave = ?",
+      [
+        "leidy.huriol@negocia.pe",
+        "$2y$10$13mAvVh6R4ZHPUIMAzgHSOZII0jy/Qx83gTM0l3j3ZjRguba0ul0W",
+      ],
+      function (err, results, fields) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      }
+    );
+  });
 }
